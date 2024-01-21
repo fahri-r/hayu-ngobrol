311eb225ff79ccf0b8d39cf4178ba3bd882ba2c6
fahri-r <00.fahri.r@gmail.com>
2024-01-21 15:27:23 +0700

    trying to fix always redirect after login

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth;
+export default withAuth({
+  callbacks: {
+    authorized: ({ req }) => {
+      // verify token and return a boolean
+      const sessionToken = req.cookies.get("next-auth.session-token");
+      if (sessionToken) return true;
+      else return false;
+    },
+  },
+});
 
 export const config = {
   matcher: ["/chat", "/chat/:id*", "/register"],
